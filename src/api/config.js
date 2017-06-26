c69c6dc4df1f6ee160c3140b5f55b240db0e3619
Dormantony <avazquez@arete.ws>
2017-06-26 10:06:16 -0500

    inicial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+appname:'Dormantony music',
+apiKey:'8385a07b22b2133ea4373d83e2145062',
+shared:'secret1c57ca7856bd5ea9869c589e24623557',
+registeredTo:'dormantony'
+}
+
+// Application name    Dormantony music
+// API key8385a07b22b2133ea4373d83e2145062
+// Shared secret1c57ca7856bd5ea9869c589e24623557
+// Registered to dormantony
