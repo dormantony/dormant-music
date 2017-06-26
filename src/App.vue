2dfdf57d4fcee0fc788a0552ca66826ac8795895
Dormantony <avazquez@arete.ws>
2017-06-26 10:12:51 -0500

    Fixes por url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #app
-  img(src='./assets/logo.png')
+  img(src='https://dormantony.github.io/dormant-music/dist/logo.png')
   h1 Essential Links2  
   select(v-model="selectedCountry")
     option(v-for="country in countries" v-bind:value='country.value') {{country.name}}
